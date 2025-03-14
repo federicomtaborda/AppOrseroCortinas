86fe53bef070f107651d35f1bc3305207875ae0e
federicomtaborda@gmail.com <federicomtaborda@gmail.com>
2025-03-14 19:25:56 -0300

    init reports

---
+++ reportes/forms.py
@@ -0,0 +1,33 @@
+
+from django import forms
+from unfold.widgets import UnfoldAdminSelectWidget, UnfoldAdminSingleDateWidget
+
+
+class TipoReporte:
+    VENTAS = 'ventas'
+
+
+TIPO_REPORTE = (
+    (TipoReporte.VENTAS, u'Ventas'),
+)
+
+
+class ReporteVentasForm(forms.Form):
+    tipo = forms.ChoiceField(label='Elija un tipo de Reporte', choices=TIPO_REPORTE, initial=TipoReporte.VENTAS,
+                             widget=UnfoldAdminSelectWidget, required=True)
+    fecha_desde = forms.DateField(
+        label=u'Fecha Desde',
+        required=True,
+        widget=UnfoldAdminSingleDateWidget(attrs={
+            "type": "date",
+        },),
+        input_formats=["%Y-%m-%d"],
+    )
+    fecha_hasta = forms.DateField(
+        label='Fecha Hasta',
+        required=True,
+        widget=UnfoldAdminSingleDateWidget(attrs={
+            "type": "date",
+        }, ),
+        input_formats=["%Y-%m-%d"],
+    )
